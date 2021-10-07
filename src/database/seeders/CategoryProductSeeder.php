0e097ea34b71a42a5e07cad7b8666b80e5d23b64
horhoj <horhoj@yandex.ru>
2021-10-08 00:07:30 +0500

    change CategoryProductSeeder

--- src/database/seeders/CategoryProductSeeder.php
+++ src/database/seeders/CategoryProductSeeder.php
@@ -29,41 +29,17 @@ class CategoryProductSeeder extends Seeder
             ],
             [
                 'category_id' => 4,
-                'product_id' => 1,
+                'product_id' => 2,
             ],
             [
                 'category_id' => 5,
-                'product_id' => 1,
+                'product_id' => 2,
             ],
             [
                 'category_id' => 6,
-                'product_id' => 1,
+                'product_id' => 2,
             ],
-            [
-                'category_id' => 7,
-                'product_id' => 1,
-            ],
-            [
-                'category_id' => 8,
-                'product_id' => 1,
-            ],
-            [
-                'category_id' => 9,
-                'product_id' => 1,
-            ],
-            [
-                'category_id' => 10,
-                'product_id' => 1,
-            ],
-            [
-                'category_id' => 11,
-                'product_id' => 1,
-            ],
-            [
-                'category_id' => 12,
-                'product_id' => 1,
-            ],
-        ];
+          ];
 
         DB::table('category_product')->insert($data);
     }
